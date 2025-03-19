a6416cd5dc7e52dcbefb8c4bcd7ec4ae439f156a
Brooke Brown <brooke.henriksson@outlook.com>
2025-03-19 10:18:20 +0100

    Completed assignment javascript and added a print to screen

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 //GET USER INPUT
 let price = prompt("Enter a price tag:")
 
+
 //REMOVE $ FROM INPUT AND CONVERT TO NUMBER
 priceNum = price.replace("$","");
 priceNum = parseFloat(priceNum);
 
+//APPLY DISCOUNT & ROUND TO NEAREST CENTS
 discount = priceNum * .90;
+discount = discount.toFixed(2);
 
+//PRINT TO CONSOLE LOG & PRINT TO USER SCREEN
 console.log(`Your new price is: $${discount}`);
+document.getElementById("discount").innerText = `Your new price is: $${discount}`;
